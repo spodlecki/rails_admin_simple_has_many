bfef0c863a021aceee9458e3a9de1bd4858cc4bd
aiman86 <najjar.aiman86@gmail.com>
2016-03-11 09:27:30 -0500

    bug fix - disabled nested_form

--- config/initializers/rails_admin_simple_has_many.rb
+++ config/initializers/rails_admin_simple_has_many.rb
@@ -2,9 +2,12 @@ module RailsAdminSimpleHasMany
   class SimpleHasManyType < RailsAdmin::Config::Fields::Types::HasManyAssociation
     # # Register field type for the type loader
     RailsAdmin::Config::Fields::Types.register(:simple_has_many, self)
-
     register_instance_option :partial do
       :form_simple_has_many
+    end
+
+    register_instance_option :nested_form do
+      false
     end
   end
 end
